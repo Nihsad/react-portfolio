d921f5cb6b0eaeb4a253e88dbfca17fdeac0cd3b
Nihsadd <nihsadd@gmail.com>
2024-06-01 18:00:28 -0600

    nav css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav>
-      <ul className="navigation-links-flex">
-        <li><Link to="/">About Me</Link></li>
-        <li><Link to="/portfolio">Portfolio</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/resume">Resume</Link></li>
+    <nav className="navigation">
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={`navigation-links-flex ${menuOpen ? 'open' : ''}`}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>About Me</Link></li>
+        <li><Link to="/portfolio" onClick={() => setMenuOpen(false)}>Portfolio</Link></li>
+        <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+        <li><Link to="/resume" onClick={() => setMenuOpen(false)}>Resume</Link></li>
       </ul>
     </nav>
   );
